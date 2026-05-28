f3d9b2f719f389a3459a80f6a231245004218fd8
shopneckmassager37-cpu <shopneckmassager37@gmail.com>
2026-05-28 17:20:19 +0300

    Add Admin route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import Navbar    from './components/Navbar'
 import Hero      from './components/Hero'
 import Expertise from './components/Expertise'
 import Work      from './components/Work'
 import Contact   from './components/Contact'
 import Footer    from './components/Footer'
+import Admin     from './components/Admin'
 
 export default function App() {
+  const isAdmin = window.location.pathname === '/admin'
+
+  if (isAdmin) {
+    return (
+      <div className="bg-[#0A0A0A] text-white font-sans">
+        <Navbar />
+        <Admin />
+      </div>
+    )
+  }
+
   return (
     <div className="bg-[#0A0A0A] text-white font-sans">
       <Navbar />
