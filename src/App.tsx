98859e1bc13e1967b506bad40c74ced33b16179a
shopneckmassager37-cpu <shopneckmassager37@gmail.com>
2026-05-28 18:07:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import Navbar       from './components/Navbar'
-import Hero         from './components/Hero'
-import Expertise    from './components/Expertise'
-import Services     from './components/Services'
-import Work         from './components/Work'
-import Process      from './components/Process'
-import Testimonials from './components/Testimonials'
-import FAQ          from './components/FAQ'
-import Contact      from './components/Contact'
-import Footer       from './components/Footer'
-import Admin        from './components/Admin'
+import Navbar    from './components/Navbar'
+import Hero      from './components/Hero'
+import Expertise from './components/Expertise'
+import Services  from './components/Services'
+import Work      from './components/Work'
+import Process   from './components/Process'
+import FAQ       from './components/FAQ'
+import Contact   from './components/Contact'
+import Footer    from './components/Footer'
+import Admin     from './components/Admin'
 
 export default function App() {
   const isAdmin = window.location.pathname === '/admin'
@@ -30,7 +29,6 @@ export default function App() {
       <Services />
       <Work />
       <Process />
-      <Testimonials />
       <FAQ />
       <Contact />
       <Footer />
